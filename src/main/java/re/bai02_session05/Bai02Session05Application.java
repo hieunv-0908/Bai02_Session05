810629d4465f949fc397dada59d9747c10d4b729
hieunv-0908 <nn650370@gmail.com>
2026-05-20 21:43:43 +0700

    first commit

---
+++ src/main/java/re/bai02_session05/Bai02Session05Application.java
@@ -0,0 +1,13 @@
+package re.bai02_session05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai02Session05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bai02Session05Application.class, args);
+    }
+
+}
---
+++ src/test/java/re/bai02_session05/Bai02Session05ApplicationTests.java
@@ -0,0 +1,13 @@
+package re.bai02_session05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bai02Session05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
